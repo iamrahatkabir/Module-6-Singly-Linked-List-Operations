737d5bc9cbf74b8adda1215bdf0bb617182d5dad
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-01 02:26:39 +0600

    Insert in tail optimized

---
+++ insert_at_tail_optimized_5.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+
+};
+
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newnode = new Node(val);
+    
+    if(head == NULL)//If Head is empty;
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    tail = tail->next;
+
+
+}
+
+
+void print_linked_list(Node* head)
+{
+    Node* temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+
+}
+
+
+
+int main()
+{
+    
+    // Node* head = NULL; //If Head is empty;
+
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->next = tail;
+
+    insert_at_tail(head, tail, 100);
+    insert_at_tail(head, tail, 500);
+    insert_at_tail(head, tail, 600);
+
+    print_linked_list(head);
+
+
+
+    return 0;
+}
